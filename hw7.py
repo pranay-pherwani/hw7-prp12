f291b76d0fa7e7cdd5c73a2199e35932f3bf06b0
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-14 17:13:28 -0400

    commented code

--- hw7.py
+++ hw7.py
@@ -1,15 +1,18 @@
 """
 hw7.py
-Name(s):
-NetId(s):
-Date:
+Name(s): Pranay Pherwani
+NetId(s): prp12
+Date: 4/14/20
 """
 
 """
 merge: the helper function for MergeSort that merges two sorted lists.
 """
 def merge(L1, L2):
+	# Initialize final list
 	L = []
+	# Append the lower value of the first value of each list
+	# and delete it until one of the lists runs out of elements
 	while len(L1)>0 and len(L2)>0:
 		if L1[0]<L2[0]:
 			L.append(L1[0])
@@ -17,6 +20,7 @@ def merge(L1, L2):
 		else:
 			L.append(L2[0])
 			del L2[0]
+	# Determine the list with remaining elements and append it to the final list
 	if len(L1)>len(L2):
 		L = L + L1
 	else:
@@ -27,11 +31,13 @@ def merge(L1, L2):
 MergeSort
 """
 def MergeSort(L):
+	# If the length is 2, swap the values if not in order
 	if len(L)==2:
 		if L[1]<L[0]:
 			temp = L[1]
 			L[1] = L[0]
 			L[0] = temp
+	# Recursively call MergeSort on the left half and right half of the list and merge them
 	elif len(L)>2:
 		mid = len(L)//2
 		left = L[:mid]
@@ -44,10 +50,12 @@ partition: the helper function for QuickSort that partitions the input
 		   list L based on the value of the pivot at L[p].
 """
 def partition(L, p):
+	# Initialize the new list
 	newL = [0]*len(L)
+	# Initialize the starting indices
 	i = 0
 	j = len(L)-1
-
+	# Place smaller elements before the pivot and larger elements after
 	for k in L:
 		if k<L[p]:
 			newL[i] = k
@@ -62,6 +70,7 @@ def partition(L, p):
 QuickSort
 """
 def QuickSort(L):
+	# Recursively call QuickSort on both parts of the partitioned set
 	if len(L)>1:
 		(newL, pIndex) = partition(L,-1)
 		L = QuickSort(newL[:pIndex])+QuickSort(newL[pIndex:])
