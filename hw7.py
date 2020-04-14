21fbf0e5bf409049372be80a5ea38888cf2c980e
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-14 12:20:50 -0400

    created repository and added file

---
+++ hw7.py
@@ -0,0 +1,31 @@
+"""
+hw7.py
+Name(s):
+NetId(s):
+Date:
+"""
+
+"""
+merge: the helper function for MergeSort that merges two sorted lists.
+"""
+def merge(L1, L2):
+    return L
+
+"""
+MergeSort
+"""
+def MergeSort(L):
+    return L
+
+"""
+partition: the helper function for QuickSort that partitions the input
+           list L based on the value of the pivot at L[p].
+"""
+def partition(L, p):
+    return (newL, i)
+
+"""
+QuickSort
+"""
+def QuickSort(L):
+    return L
